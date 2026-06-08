409cd7be5f8692d148c9e9fbc0c0c55345277594
BryanCC7 <bryan.correa2201@alumnos.ubiobio.cl>
2026-06-08 09:24:06 -0400

    Falta Paginación

--- app-standalone/src/app/app.routes.ts
+++ app-standalone/src/app/app.routes.ts
@@ -4,11 +4,18 @@ import { Welcome } from './features/home/welcome/welcome';
 import { PageNotFound } from './features/not-found/page-not-found/page-not-found';
 import {Login} from './features/auth/components/login/login';
 
+import { Number } from './features/numbers/components/number/number';
+import { User } from './features/users/components/user/user';
+
 import { loginGuard } from './features/auth/guards/login-guard';
 
 export const routes: Routes = [
+
+  
   { path: 'home', component: Welcome, canActivate: [loginGuard] },
   { path: 'products', component: ProductComponent, canActivate:[loginGuard] },
+  { path: 'numbers', component: Number, canActivate: [loginGuard] },
+  { path: 'users', component: User, canActivate:[loginGuard]},
   { path: 'login', component:Login},
   
   { path: '', redirectTo: 'home', pathMatch: 'full' },
